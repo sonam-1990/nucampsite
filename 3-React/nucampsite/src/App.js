f054326028e1a08473ad5f09294e4de860be5e13
sonam-1990 <83310926+sonam-1990@users.noreply.github.com>
2021-06-24 17:27:57 -0400

    React Router

--- 3-React/nucampsite/src/App.js
+++ 3-React/nucampsite/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Main  from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 
@@ -8,9 +9,11 @@ class App extends Component {
    
   render() {
       return (
+       < BrowserRouter>
           <div className="App">
             <Main />
           </div>
+         </BrowserRouter>
       );
   };
 }
---
+++ 3-React/nucampsite/src/components/HomeComponent.js
@@ -0,0 +1,10 @@
+import REACT from 'react';
+function Home(props){
+    return(
+        <div className="container">
+            <h4>Home</h4>
+            </div>
+
+    );
+}
+export default Home;
