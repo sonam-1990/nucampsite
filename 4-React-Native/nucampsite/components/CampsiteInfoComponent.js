b913de0f2d213b57b3e300920ff4da564657e363
sonam-1990 <83310926+sonam-1990@users.noreply.github.com>
2021-07-28 00:29:07 -0400

    added Icon,Comments & favorites

--- 4-React-Native/nucampsite/components/CampsiteInfoComponent.js
+++ 4-React-Native/nucampsite/components/CampsiteInfoComponent.js
@@ -1,10 +1,11 @@
 import React,{ Component } from 'react';
 import { Text, View, ScrollView, FlatList } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card ,Icon } from 'react-native-elements';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
 
-function RenderComment({comment}){
+
+function RenderComments({comments}){
     const renderCommentItem = ({item}) => {
         return (
             <View style={{margin: 10}}>
@@ -16,8 +17,8 @@ function RenderComment({comment}){
     };
 
     return(
-        <Card title="Comments">
-             <FlatList
+        <Card title='Comments'>
+            <FlatList
                 data={comments}
                 renderItem={renderCommentItem}
                 keyExtractor={item => item.id.toString()}
@@ -27,7 +28,8 @@ function RenderComment({comment}){
 }
 
 
-function RenderCampsite({campsite}) {
+function RenderCampsite(props) {
+    const {campsite} = props;
     if (campsite) {
         return (
             <Card 
@@ -37,7 +39,20 @@ function RenderCampsite({campsite}) {
                 <Text style={{margin: 10}}>
                     {campsite.description}
                 </Text>
+                <Icon
+                    name={props.favorite ? 'heart' : 'heart-o'}
+                    type='font-awesome'
+                    color='#f50'
+                    raised
+                    reverse
+                    onPress={() => props.favorite ? 
+                        console.log('Already set as a favorite') : props.markFavorite()}
+                   
+                />
             </Card>
+          
+            
+            
         );
     }
     return <View />;
@@ -49,21 +64,27 @@ class CampsiteInfo extends Component {
         super(props);
         this.state = {
             campsites: CAMPSITES,
-            comments:COMMENTS
+            comments: COMMENTS,
+            favorite: false
         };
     }
-
+    markFavorite() {
+        this.setState({favorite: true});
+    }
     static navigationOptions = {
         title: 'Campsite Information'
     }
     render() {
         const campsiteId = this.props.navigation.getParam('campsiteId');
         const campsite = this.state.campsites.filter(campsite => campsite.id === campsiteId)[0];
-        const comment =this.state.comments.filter(comment =>comment.campsiteId ===campsiteId);
+        const comments =this.state.comments.filter(comment =>comment.campsiteId ===campsiteId);
         return(
             <ScrollView>
-               <RenderCampsite campsite={campsite} />
-               <RenderComment comment={comment} />
+               <RenderCampsite campsite={campsite}
+                favorite={this.state.favorite}
+                markFavorite={() => this.markFavorite()}
+                 />
+               <RenderComments comments={comments} />
             </ScrollView>
         );
     };
