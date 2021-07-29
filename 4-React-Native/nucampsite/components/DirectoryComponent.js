c65ea21119904b22efcf30baa1ee5314da5ae9c8
sonam-1990 <83310926+sonam-1990@users.noreply.github.com>
2021-07-29 10:51:54 -0400

    Adding Activity Indicator

--- 4-React-Native/nucampsite/components/DirectoryComponent.js
+++ 4-React-Native/nucampsite/components/DirectoryComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList } from 'react-native';
+import { FlatList,View,Text } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -32,6 +32,16 @@ class Directory extends Component {
                 />
             );
         };
+        if (this.props.campsites.isLoading) {
+            return <Loading />;
+        }
+        if (this.props.campsites.errMess) {
+            return (
+                <View>
+                    <Text>{this.props.campsites.errMess}</Text>
+                </View>
+            );
+        }
 
         return (
             <FlatList
