e1aee1a963b38a37415afc2050ce8253c464a01c
sonam-1990 <83310926+sonam-1990@users.noreply.github.com>
2021-08-02 23:13:46 -0400

    Add FavoritesComponent.

--- 4-React-Native/nucampsite/components/MainComponent.js
+++ 4-React-Native/nucampsite/components/MainComponent.js
@@ -4,6 +4,8 @@ import CampsiteInfo from './CampsiteInfoComponent';
 import Home from './HomeComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
+
+import Favorites from './FavoritesComponent';
 import Reservation from './ReservationComponents';
 import Constants from 'expo-constants';
 import { View, Platform, StyleSheet ,Text, ScrollView, Image} from 'react-native';
@@ -145,6 +147,29 @@ const  ReservationNavigator= createStackNavigator(
     }
 
 );
+const  FavoritesNavigator= createStackNavigator(
+    {
+        Favorites:{screen: Favorites}
+    },
+    {
+        defaultNavigationOptions: ({ navigation }) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='heart'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+
+);
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         <SafeAreaView 
@@ -198,6 +223,20 @@ const MainNavigator = createDrawerNavigator(
                 drawerIcon: ({tintColor}) => (
                     <Icon
                         name='tree'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        Favorites: { 
+            screen: FavoritesNavigator,
+            navigationOptions: {
+                drawerLabel: 'My Favorites',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='heart'
                         type='font-awesome'
                         size={24}
                         color={tintColor}
