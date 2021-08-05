c705c55c49fd50519eaecf3ef29d7e188b78fbfe
sonam-1990 <83310926+sonam-1990@users.noreply.github.com>
2021-08-05 10:35:18 -0400

    Gesture with animation

--- 4-React-Native/nucampsite/components/CampsiteInfoComponent.js
+++ 4-React-Native/nucampsite/components/CampsiteInfoComponent.js
@@ -51,9 +51,15 @@ function RenderComments({ comments }) {
 
 function RenderCampsite(props) {
    const { campsite } = props;
+   const view = React.createRef();
    const recognizeDrag =({dx})=>(dx <-200)? true:false;
    const panResponder=PanResponder.create({
-    onStartShouldSetPanResponder:() =>true ,
+    onStartShouldSetPanResponder:() => true,
+    onPanResponderGrant: () => {
+        view.current.rubberBand(1000)
+        .then(endState => console.log(endState.finished ? 'finished' : 'canceled'));
+    },
+  
     onPanResponderEnd:(e,gestureState) => {
         console.log('pan responder end', gestureState);
         if(recognizeDrag(gestureState))
@@ -87,6 +93,7 @@ function RenderCampsite(props) {
              animation='fadeInDown'
               duration={2000}
                delay={1000}
+               ref={view}
                {...panResponder.panHandlers}>
                 <Card
                     featuredTitle={campsite.name}
