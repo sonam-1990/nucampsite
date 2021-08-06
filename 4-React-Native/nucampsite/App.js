7517b07c47121ef4382d628cf110f52235a49e92
sonam-1990 <83310926+sonam-1990@users.noreply.github.com>
2021-08-06 12:18:39 -0400

    persist

--- 4-React-Native/nucampsite/App.js
+++ 4-React-Native/nucampsite/App.js
@@ -5,18 +5,17 @@ import { ConfigureStore } from './redux/configureStore';
 import { PersistGate } from 'redux-persist/es/integration/react';
 import Loading from './components/LoadingComponent';
 
-const { persistor, store } = ConfigureStore();
-
+const {persistor,store}  = ConfigureStore();
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <PersistGate
-        loading={<Loading />}
-        persistor={persistor}>
-        <Main />
-      </PersistGate>
-    </Provider>
-
-  );
+    return (
+     <Provider store={store}>
+         <PersistGate
+             loading={<Loading />}
+             persistor={persistor}>
+             <Main />
+         </PersistGate>
+     </Provider>
+     
+    );
 }
