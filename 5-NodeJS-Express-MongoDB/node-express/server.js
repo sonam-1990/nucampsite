49e0c2cd22ffaa94142506441160975d6f969a56
sonam-1990 <83310926+sonam-1990@users.noreply.github.com>
2021-08-27 14:09:12 -0400

    Use Morgan middleware for logging & serve static files.

--- 5-NodeJS-Express-MongoDB/node-express/server.js
+++ 5-NodeJS-Express-MongoDB/node-express/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();   //return express server application
 
+app.use(morgan('dev')) ;   //   Morgan log in Dev. Version. 
+app.use(express.static(__dirname +'/public'));     //take absolute path .
+
 app.use((req ,res) =>{                     //set up server
-    console.log(req.headers);
+  
     res.statusCode=200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
