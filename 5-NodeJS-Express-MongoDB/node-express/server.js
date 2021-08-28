a0981898722098f55f921c1cd1ee9b570e41d3a2
sonam-1990 <83310926+sonam-1990@users.noreply.github.com>
2021-08-27 23:32:05 -0400

    Set up REST API endpoints using Express routing method

--- 5-NodeJS-Express-MongoDB/node-express/server.js
+++ 5-NodeJS-Express-MongoDB/node-express/server.js
@@ -4,12 +4,58 @@ const morgan = require('morgan');
 const hostname = 'localhost';
 const port = 3000;
 
-const app = express();   //return express server application
+const app = express();                             //return express server application
 
-app.use(morgan('dev')) ;   //   Morgan log in Dev. Version. 
-app.use(express.static(__dirname +'/public'));     //take absolute path .
+app.use(morgan('dev')) ;                              //   Morgan log in Dev. Version. 
+app.use(express.json());
 
-app.use((req ,res) =>{                     //set up server
+app.all('/campsites',(req,res,next) =>{
+    res.statusCode=200;
+    res.setHeader('Content-Type','text/plain');
+    next();
+});
+
+app.get('/campsites',(req,res) => {
+    res.end('Will send all the Campsites to you');
+
+});
+
+app.post('/campsites',(req,res)=>{
+    res.end(`Will add the campsite:${req.body.name} with description: ${req.body.description}`)
+
+});
+app.put('/campsites',(req,res) =>{
+    res.statusCode=403;
+    res.end('PUT operation not supported on /campsites');
+});
+
+app.delete('/campsites',(req,res) =>{
+    res.end('Deleting all campsites');
+});
+
+app.get('/campsites/:campsiteId',(req,res) => {
+    res.end(`Will send details of the campsite:${req.params.campsiteId} to you`);
+
+});
+
+app.post('/campsites/:campsiteId',(req,res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+app.put('/campsites/:campsiteId', (req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+});
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
+
+app.use(express.static(__dirname +'/public'));        //take absolute path .
+
+app.use((req ,res) =>{                                     //set up server
   
     res.statusCode=200;
     res.setHeader('Content-Type', 'text/html');
