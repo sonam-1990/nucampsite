b0f38503450997fdf247a17fa322755e68da6e8f
sonam-1990 <83310926+sonam-1990@users.noreply.github.com>
2021-08-28 13:03:33 -0400

    Added PromotionRouter and partnerRouter

--- 5-NodeJS-Express-MongoDB/node-express/server.js
+++ 5-NodeJS-Express-MongoDB/node-express/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
-const campsiteRouter =require('./routes/campsiteRouter');
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -10,7 +12,8 @@ const app = express();                             //return express server appli
 app.use(morgan('dev')) ;                              //   Morgan log in Dev. Version. 
 app.use(express.json());
 app.use('/campsites',campsiteRouter);
-
+app.use('/promotions',promotionRouter);
+app.use('/partners',partnerRouter);
 
 
 app.use(express.static(__dirname +'/public'));        //take absolute path .
