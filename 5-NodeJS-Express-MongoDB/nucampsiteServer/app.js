c87c6c1c8f243ca19eefd87086831e4d90d253b8
sonam-1990 <83310926+sonam-1990@users.noreply.github.com>
2021-09-09 10:29:00 -0400

    Passport:Session based authentication.

--- 5-NodeJS-Express-MongoDB/nucampsiteServer/app.js
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
-
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -45,25 +46,22 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req,res,next){
-  console.log(req.session);
-  if(!req.session.user){
-  
+  console.log(req.user);
+
+  if(!req.user){                          //no session loaded for the client 
     const err = new Error('You are not authenticated!');
-    err.status=401;
+    err.status = 401;
     return next(err);
-  } else{
-    if (req.session.user === 'authenticated') {
-      return next();
-  } else {
-      const err = new Error('You are not authenticated!');
-      err.status = 401;
-      return next(err);
-        }
-    }
+  }else{
+    return next();                       //send to next middleware.
+  }
 
 }
 app.use(auth);
